0a1d62e95a38298a9f2a2127763fc4c8542d353b
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-30 10:20:33 -0700

    almost done

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 #Ethan Blanco, Music Festival, Venue Management
 
-venue_list = []
+venue_list = ("Nothing")
 
 def venue_management(): #This function is for assigning stages with artists on a certain time for the music festival
     
     while True:
         venue_verifi = input(f"Is this done? Type yes or no to answer {venue_list}\n") #Lets the user see the current progress on the venue management
-        if venue_verifi == "yes" and "Yes" and "YES": #As of right now, I am not sure why the "Yes" and "YES" don't work, it's probably my else statement though.
+        if venue_verifi == "yes" and venue_verifi == "Yes" and venue_verifi == "YES": #As of right now, I am not sure why the "Yes" and "YES" don't work, it's probably my else statement though.
             print("Great! You should be done here now, if you haven't, try and see if all the other managements are done!")
             break
-        elif venue_verifi == "no" and "No" and "NO":
+        elif venue_verifi == "no" and venue_verifi == "No" and venue_verifi == "NO":
             print("Let's get started then!")
-            venue_start = int(input("""Would you like to create, edit, or remove an existing venue list?
+            venue_start = input("""Would you like to create, edit, or remove an existing venue list?
                                 1. Create a new venue list
                                 2. Edit an already existing venue list
                                 3. Remove a venue list
                                 4. View venue list
-                                5. Return/Backspace\n""")) #Let's the user do what they want with the venue related stuff, anything that can be managed is available.
-            if venue_start == 1:
+                                5. Return/Backspace\n""") #Let's the user do what they want with the venue related stuff, anything that can be managed is available.
+            if venue_start == "1":
+                new_ven_name = input("What would you like to name this new venue list? Please include the name, stage and time.")
+                venue_list = []
+                venue_list.append(new_ven_name)
+                venue_list.pop(1)
+                print(venue_list)
+            elif venue_start == "2":
                 pass
-            elif venue_start == 2:
+            elif venue_start == "3":
                 pass
-            elif venue_start == 3:
-                pass
-            elif venue_start == 4:
+            elif venue_start == "4":
                 print("Here you go!")
                 print(venue_list)
                 continue
-            elif venue_start == 5:
+            elif venue_start == "5":
                 print("Let's head back then, back to the start of the venue management!")
                 continue
             else:
                 print("This doesn't work, try entering a registered number instead!")
                 continue
-        elif venue_verifi == "" and " ":
+        elif venue_verifi == "" and venue_verifi == " ":
             print("You wrote nothing! Please try again.")
             continue
         else:
@@ -64,10 +68,10 @@ def main():
             pass
         elif user_interface == 6:
             last_verifi = input("Are you sure you're done? Please reply with a simple yes or no\n")
-            if last_verifi == "yes" and "Yes" and "YES":
+            if last_verifi == "yes" and last_verifi == "Yes" and last_verifi == "YES":
                 print("Thank you for your work today! It's time to head on home!")
                 break
-            if last_verifi == "no" and "No" and "NO":
+            elif last_verifi == "no" and last_verifi == "No" and last_verifi == "NO":
                 print("It's time to head on back then!")
                 continue
         elif user_interface == 7:
